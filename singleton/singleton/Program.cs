710fa94fa0126f4721e74e979b3be3929173bb2e
rmzmaher <rmz.aziz@gmail.com>
2016-11-27 13:12:58 +0200

    Implementing Singleton in C#
    - Why Singleton ?
    This pattern involves a single class which is responsible to create an
    object while making sure that only single object gets created.
    - Advantage
    The instantiation is not performed until an object asks for an instance;
    this approach is referred to as lazy instantiation. Lazy instantiation
    avoids instantiating unnecessary singletons when the application starts.
    - Disadvantage
    It is not safe for multithreaded environments. If separate threads of
    execution enter the Instance property method at the same time, more that
    one instance of the Singleton object may be created.

---
+++ singleton/singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            singleton obj = singleton.initialize;
+        }
+    }
+
+    public class singleton
+    {
+        //make the constructor private so that this class cannot be
+        //instantiated
+        private singleton() {}
+        private static singleton instance;
+        //create and Get only object one object
+        public static singleton initialize
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new singleton();
+                }
+                return instance;
+            }
+        }
+    }
+
+}
